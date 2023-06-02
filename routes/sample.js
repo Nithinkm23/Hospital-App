e1c34fa1a71770ac3ebfef0bb5c18e77958c15b3
NithinKM23 <nithinkm55@gmail.com>
2023-06-02 23:38:28 +0530

    Update sample.js

--- routes/sample.js
+++ routes/sample.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }))
+//fs
 const fs = require("fs")
 
 fs.readFile("users.json", function (err, data) {
@@ -10,8 +11,27 @@ fs.readFile("users.json", function (err, data) {
     const users = JSON.parse(data);
     console.log(users);
 })
+//GET
 router.get("/", (req, res) => {
     res.send(users);
 });
+//POST
+router.post('/add', (req, res) => {
+    console.log(req.body)
+    users.push('');
+    res.send(users);
+})
+//UPDATE
+router.put("/update/:ind", (req, res) => {
+    const index = req.params.ind;
+    console.log(`Updated index : ${index}`);
+    res.send("Updated")
+})
+//DELETE
+router.delete("/delete/:ind", (req, res) => {
+    const index = req.params.ind;
+    console.log(`Deleted index : ${index}`);
+    res.send("Deleted")
+})
 
 module.exports = router;
